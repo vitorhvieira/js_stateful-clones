900ce0f770ebcf83227743c8991b773dca20db2f
Vitor <vitorhugovieirabraga@gmail.com>
2025-01-26 17:46:58 -0300

    Fix code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,27 +8,30 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
-  let newState = { ...state };
+  let currentState = { ...state };
   const stateHistory = [];
 
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        newState = { ...newState, ...action.extraData };
+        currentState = { ...currentState, ...action.extraData };
         break;
 
       case 'removeProperties':
         for (const key of action.keysToRemove) {
-          delete newState[key];
+          delete currentState[key];
         }
         break;
 
       case 'clear':
-        newState = {};
+        currentState = {};
+        break;
+
+      default:
         break;
     }
 
-    stateHistory.push({ ...newState });
+    stateHistory.push({ ...currentState });
   }
 
   return stateHistory;
